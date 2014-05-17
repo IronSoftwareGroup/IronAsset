fb03116ef1f2a6bd5699b492fd278334f6297c56
bruno <bruno@192.168.2.108>
2014-05-17 15:46:56 +0200

    populate.sql script

---
+++ populate.sql
@@ -0,0 +1,49 @@
+INSERT INTO lov values( 'ASSET_TYPE1', 'OS', 'Operating System');
+INSERT INTO lov values( 'ASSET_TYPE1', 'HW', 'Hardware');
+INSERT INTO lov values( 'ASSET_TYPE1', 'SW', 'Software');
+
+INSERT INTO lov values( 'ASSET_TYPE2', 'Monitor', 'PC Monitor');
+INSERT INTO lov values( 'ASSET_TYPE2', 'Printer', 'Printer');
+INSERT INTO lov values( 'ASSET_TYPE2', 'Router', 'Router');
+INSERT INTO lov values( 'ASSET_TYPE2', 'Switch', 'Switch');
+INSERT INTO lov values( 'ASSET_TYPE2', 'Cable', 'Cable');
+INSERT INTO lov values( 'ASSET_TYPE2', 'Tablet', 'Tablet');
+INSERT INTO lov values( 'ASSET_TYPE2', 'Smartphone', 'Smartphone');
+INSERT INTO lov values( 'ASSET_TYPE2', 'PC', 'Personal Computer');
+INSERT INTO lov values( 'ASSET_TYPE2', 'Workstation', 'Workstation');
+INSERT INTO lov values( 'ASSET_TYPE2', 'Server', 'Server');
+INSERT INTO lov values( 'ASSET_TYPE2', 'Thin Client', 'Thin Client');
+INSERT INTO lov values( 'ASSET_TYPE2', 'PDA', 'PDA');
+
+INSERT INTO lov values( 'ASSET_TYPE2', 'Network', 'Network');
+INSERT INTO lov values( 'ASSET_TYPE2', 'Productivity', 'Productivity');
+INSERT INTO lov values( 'ASSET_TYPE2', 'Audio', 'Audio');
+INSERT INTO lov values( 'ASSET_TYPE2', 'Video', 'Video');
+INSERT INTO lov values( 'ASSET_TYPE2', 'Internet', 'Internet');
+INSERT INTO lov values( 'ASSET_TYPE2', 'Uitility', 'Utility');
+INSERT INTO lov values( 'ASSET_TYPE2', 'Business', 'Business');
+
+INSERT INTO lov values( 'ASSET_TYPE3', 'Inventory', 'Inventory');
+INSERT INTO lov values( 'ASSET_TYPE3', 'Sales', 'Sales');
+INSERT INTO lov values( 'ASSET_TYPE3', 'Finance', 'Finance');
+INSERT INTO lov values( 'ASSET_TYPE3', 'Purchase', 'Purchase');
+INSERT INTO lov values( 'ASSET_TYPE3', 'Manufacturing', 'Manufacturing');
+INSERT INTO lov values( 'ASSET_TYPE3', 'EDP', 'EDP');
+
+
+INSERT INTO lov values( 'CURRENCY', 'EUR', 'Euro');
+INSERT INTO lov values( 'CURRENCY', 'USD', 'Usd');
+
+INSERT INTO lov values( 'UOM', 'EA', 'Each'
+INSERT INTO lov values( 'UOM', 'Per Socket', 'Software is licenzed per single socket');
+INSERT INTO lov values( 'UOM', 'Per Core', 'Software is licenzed per single core');
+INSERT INTO lov values( 'UOM', 'Per User', 'Software is licensed per single user');
+
+INSERT INTO lov values( 'VENDOR', 'Microsoft', 'Microsoft');
+INSERT INTO lov values( 'VENDOR', 'Oracle', 'Oracle');
+INSERT INTO lov values( 'VENDOR', 'IBM', 'International Business Machine');
+INSERT INTO lov values( 'VENDOR', 'Apple', 'Apple');
+
+INSERT INTO global_properties values( 'EMAIL', 'CC', 'admin@ironsg.com','Copy email address);
+INSERT INTO global_properties values( 'EMAIL', 'SENDER', 'admin@ironsg.com','Email sender);
+INSERT INTO global_properties values( 'EMAIL', 'RECEIVER', 'admin@ironsg.com','Email receiver);
